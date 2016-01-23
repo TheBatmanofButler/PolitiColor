f337cb03d233c9348c35254f2b84a03c35ae2c5c
Ganesh Ravichandran <gr2483@columbia.edu>
2016-01-22 21:53:13 -0500

    added brill and sentiment

--- brill.js
+++ brill.js
@@ -5,6 +5,8 @@
 
 	Description: Brill
 **/
+
+// part-of-speech brill library
 var pos = require('pos');
 
 module.exports = {
@@ -14,4 +16,15 @@ module.exports = {
 		var taggedWords = tagger.tag(words);
 	    callback(taggedWords)
 	}
+}
+
+var main = function(response) {
+    var words = new pos.Lexer().lex(response);
+	var tagger = new pos.Tagger();
+	var taggedWords = tagger.tag(words);
+	console.log(taggedWords);
+}
+
+if (require.main === module) {
+	main("Thursday evening, Sen. Bernie Sanders’ presidential campaign account tweeted out a promise that is likely to bring joy to anyone dismayed by the increasing flood of money seeking to influence American elections.");
 }
--- sentiment.js
+++ sentiment.js
@@ -1,13 +1,15 @@
 /**
-	@author: Amol Kapoor
+	@author: Ganesh Ravichandran
 	@version: 0.1
 	@date: 1-22-16
 
 	Description: Sentiment
 **/
 
+var sentiment = require('sentiment');
+
 module.exports = {
 	process: function(response, callback) {
-		
+		return sentiment(response).score;
 	}
 }
