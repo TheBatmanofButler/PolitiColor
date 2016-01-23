9e98a4e0dd7677bc0259a00c43f03ab0ea82753b
Ganesh Ravichandran <gr2483@columbia.edu>
2016-01-22 20:52:23 -0500

    brill done

--- brill.js
+++ brill.js
@@ -1,13 +1,17 @@
 /**
-	@author: Amol Kapoor
+	@author: Ganesh Ravichandran
 	@version: 0.1
 	@date: 1-22-16
 
 	Description: Brill
 **/
+var pos = require('pos');
 
 module.exports = {
 	process: function(response, callback) {
-		
+	    var words = new pos.Lexer().lex(response);
+		var tagger = new pos.Tagger();
+		var taggedWords = tagger.tag(words);
+	    callback(taggedWords)
 	}
 }
