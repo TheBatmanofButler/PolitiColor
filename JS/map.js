04b722354f97bdecc03e9db8a158c43edba2d197
Amol Kapoor <theahura@gmail.com>
2016-01-23 22:16:18 -0500

    Signed-off-by: Amol Kapoor <theahura@gmail.com>

--- JS/map.js
+++ JS/map.js
@@ -1,25 +1,24 @@
+function ready(error, us, congress, callback) {
 
-var width = Math.floor($(window).width() - 30),
-    height = Math.floor($(window).height());
+  var width = Math.floor($('.map-container').width() - 30),
+      height = Math.floor($('.map-container').height());
 
-var rateById = d3.map();
+  var rateById = d3.map();
 
-var quantize = d3.scale.quantize()
-    .domain([0, .15])
-    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+  var quantize = d3.scale.quantize()
+      .domain([0, .15])
+      .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
 
-var projection = d3.geo.albersUsa()
-    .scale(width)
-    .translate([width / 2, height / 2]);
+  var projection = d3.geo.albersUsa()
+      .scale(width)
+      .translate([width / 2, height / 2]);
 
-var path = d3.geo.path()
-    .projection(projection);
+  var path = d3.geo.path()
+      .projection(projection);
 
-var svg = d3.select(".subtitle").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-function ready(error, us, congress) {
+  var svg = d3.select(".map-container").append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
   var features = svg.append("g");    
     /**features.call(zoom)
@@ -52,11 +51,9 @@ function ready(error, us, congress) {
       .attr("class", "state-boundaries")
       .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
       .attr("d", path);
+
+  callback();
 }
-
-ready(null, us, congress);
-
-d3.select(self.frameElement).style("height", height + "px");
 
 //-----------------------------------------------------------------------------------------------------------------
 
@@ -96,5 +93,12 @@ socket.on('serverToClient', function(data){
 
 
 $(document).ready(function() {
-  $('html').fadeIn();
+
+  $('html').fadeIn('slow');
+
+
+  ready(null, us, congress, function() {
+  });
+
+  d3.select(self.frameElement).style("height", height + "px");
 });
