a0cbeac491708bb3633fcc80507fc8cb41792d48
hongderek <hong.derek.j@gmail.com>
2016-01-23 19:55:03 -0500

    ASDF

--- twitterDataParser.js
+++ twitterDataParser.js
@@ -1,2 +1,34 @@
 // Derek Hong 2016
-// 
+// Twitter data parser (test run for rolling Avg Server)
+
+
+var output = []
+
+var fs = require('fs');
+var csv = require('csv');
+
+var reader = fs.createReadStream('stream1.csv');
+
+reader.on('readable', () => {
+	var csvRead = reader.read();
+	csv.parse(csvRead, function(err, data){
+		console.log(csvRead)
+		console.log(err);
+		//output.push(data);
+		//console.log(data);
+	});
+});
+
+reader.on('end', () => {
+  	console.log('end');
+});
+
+
+
+
+
+
+
+
+
+
